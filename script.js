b6256cff3d4e11a48c19feed0f901086c32528fb
hmimid-ahmed <ahmedhmimid177@gmail.com>
2024-05-18 22:56:30 +0100

    removing a debugger

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const resultsDiv = document.getElementById("results-div");
 const findValidNumber = (number) => {
 	const validNumberUs =
 		/(((^[1]{1})\s\d{3}-\d{3}-\d{3})|((^[1]{1})\s[(]\d{3}[)]\s\d{3}-\d{3})|((^[1]{1})[(]\d{3}[)]\d{3}-\d{3})|((^[1]{1})\s\d{3}\s\d{3}\s\d{3})|((^[5|1]{1})\d{8})|((^[5|1]{1})\d{2}-\d{3}-\d{3})|((^[(]{1})\d{3}[)]\d{3}-\d{3}))(\d{1}$)/g;
-	// console.log(number.match(validNumberUs));
+
 	return number.match(validNumberUs) !== null ? true : false;
 };
 
